c61e2d731508b74ae3c2cd4080c4abac40e725d9
Cjacker <jianzhong.huang@i-soft.com.cn>
2015-12-02 15:41:11 +0800

    Update and Fix

--- vokoscreen/vokoscreen.spec
+++ vokoscreen/vokoscreen.spec
@@ -1,26 +1,41 @@
 Summary: Screencasting
 Name: vokoscreen
-Version: 2.4.3
-Release: 2.git
+Version: 2.4.8
+Release: 3 
 License: GPL-2.0
 URL: http://www.kohaupt-online.de/hp
 #https://github.com/vkohaupt/vokoscreen
+Source0: vokoscreen-%{version}-beta.tar.gz
+
+#Updated zh CN translation
+Source1: vokoscreen_zh_CN.ts
+
+Patch0: we-do-not-have-mp3lame.patch
+Patch1: vokoscreen-add-our-bomi-player.patch
+Patch2: vokoscreen-really-quit.patch
+Patch3: vokoscreen-update-hide-show-window-status.patch
+Patch4: vokoscreen-add-zh_CN-info-to-desktop.patch
+Patch5: vokoscreen-disable-check-new-version.patch
+Patch6: vokoscreen-change-window-title.patch
+ 
 Requires: ffmpeg 
+Requires: bomi
 
-BuildRequires: qt5-qtbase-devel alsa-lib-devel
-
-Source: %{name}.tar.gz
+BuildRequires: qt5-qtbase-devel qt5-qtx11extras-devel
+BuildRequires: alsa-lib-devel libv4l-devel
 
 %description
-vokoscreen is an easy to use screencast creator to record educational
-videos, live recordings of browser, installation, videoconferences, etc.
+vokoscreen is an easy to use screencast creator to record educational videos,
+live recordings of browser, installation, videoconferences, etc.
 
 %prep
-%setup -n %{name}
-
+%autosetup -n %{name}-%{version}-beta -p1
+rm -rf language/vokoscreen_zh_CN.ts
+cp %{SOURCE1} language
+ 
 %build
 qmake-qt5
-make
+make %{?_smp_mflags}
 
 %install
 install -D -m 755 vokoscreen %{buildroot}%{_bindir}/vokoscreen
@@ -36,8 +51,11 @@ install -D -m 644 man/man1/%{name}.1.gz %{buildroot}%{_mandir}/man1/%{name}.1.gz
 %{_mandir}/man1/%{name}.1.gz
 
 %changelog
+* Wed Dec 02 2015 Cjacker <cjacker@foxmail.com> - 2.4.8-3
+- Rebuilt
+
+* Wed Dec 02 2015 Cjacker <cjacker@foxmail.com> - 2.4.8-2
+- Update to 2.4.8 Beta
+- Add our fixes(a lot of)
 * Tue Oct 27 2015 Cjacker <cjacker@foxmail.com> - 2.4.3-2.git
 - Rebuild
-
-* Sun Mar 21 2012 Volker Kohaupt <vkohaupt@freenet.de> 1.6.3
-- new Version
---
+++ vokoscreen/vokoscreen_zh_CN.ts
@@ -0,0 +1,351 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AlsaBusyDialog</name>
+    <message>
+        <source>Device is busy</source>
+        <translation>设备忙</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <source>Device</source>
+        <translation>设备</translation>
+    </message>
+    <message>
+        <source>Used by</source>
+        <translation>被以下应用占用：</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>Show Click Settings</source>
+        <translation>显示“鼠标点击效果”设置</translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation>标准</translation>
+    </message>
+</context>
+<context>
+    <name>MailDialog</name>
+    <message>
+        <source>Mail last Video</source>
+        <translation>作为邮件附件发送最近录制的视频</translation>
+    </message>
+    <message>
+        <source>Mail one or more selected Video</source>
+        <translation>选择视频作为邮件附件发送</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>发送</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>NoPlayerDialog</name>
+    <message>
+        <source>Please install a videoplayer</source>
+        <translation>请安装媒体播放器</translation>
+    </message>
+</context>
+<context>
+    <name>QvkMail</name>
+    <message>
+        <source>You find the Video as attachment</source>
+        <translation>选择这个视频作为邮件附件</translation>
+    </message>
+    <message>
+        <source>Select one or more files</source>
+        <translation>选择一个或多个文件</translation>
+    </message>
+</context>
+<context>
+    <name>QvkWebcamWindow</name>
+    <message>
+        <source>Border</source>
+        <translation>边框</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <source>Show over fullscreen</source>
+        <translation>全屏显示</translation>
+    </message>
+    <message>
+        <source>Right click for menu</source>
+        <translation>右键打开菜单</translation>
+    </message>
+    <message>
+        <source>User-defined</source>
+        <translation>用户自定义</translation>
+    </message>
+</context>
+<context>
+    <name>WebcamBusyDialog</name>
+    <message>
+        <source>Device is busy</source>
+        <translation>设备忙</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <source>Device</source>
+        <translation>设备</translation>
+    </message>
+    <message>
+        <source>Used by</source>
+        <translation>被以下应用占用：</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+</context>
+<context>
+    <name>regionselection</name>
+    <message>
+        <source>double-click to maximize</source>
+        <translation>双击最大化</translation>
+    </message>
+</context>
+<context>
+    <name>screencast</name>
+    <message>
+        <source>Voko Screen Cast</source>
+        <translation>Voko屏幕录制</translation>
+    </message>
+    <message>
+        <source>Fullscreen</source>
+        <translation>全部屏幕</translation>
+    </message>
+    <message>
+        <source>Window</source>
+        <translation>某一窗口</translation>
+    </message>
+    <message>
+        <source>Area</source>
+        <translation>选定区域</translation>
+    </message>
+    <message>
+        <source>Magnification</source>
+        <translation>区域放大</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation>录音</translation>
+    </message>
+    <message>
+        <source>Alsa</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Pulse</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <source>Codec</source>
+        <translation>编码器</translation>
+    </message>
+    <message>
+        <source>Do not record mouse cursor</source>
+        <translation>不要抓取鼠标光标</translation>
+    </message>
+    <message>
+        <source>Videopath</source>
+        <translation>视频保存路径</translation>
+    </message>
+    <message>
+        <source>Player</source>
+        <translation>播放器</translation>
+    </message>
+    <message>
+        <source>Vokoscreen minimized when recording starts</source>
+        <translation>开始录制时最小化本窗口</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>停止</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>播放</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>发送</translation>
+    </message>
+    <message>
+        <source>Recording time</source>
+        <translation>录制时长</translation>
+    </message>
+    <message>
+        <source>Size in KB</source>
+        <translation>大小(KB)</translation>
+    </message>
+    <message>
+        <source>Recording screensize</source>
+        <translation>录制屏幕大小</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>格式</translation>
+    </message>
+    <message>
+        <source>Settings fps</source>
+        <translation>设定帧率</translation>
+    </message>
+    <message>
+        <source>Open Directory</source>
+        <translation>打开目录</translation>
+    </message>
+    <message>
+        <source>Countdown</source>
+        <translation>倒计时</translation>
+    </message>
+    <message>
+        <source>Play last Video</source>
+        <translation>播放最近录制的视频</translation>
+    </message>
+    <message>
+        <source>Send Video</source>
+        <translation>发送视频</translation>
+    </message>
+    <message>
+        <source>Select one or more devices</source>
+        <translation>选择一个或多个设备</translation>
+    </message>
+    <message>
+        <source>Actual frames per second</source>
+        <translation>实际帧率</translation>
+    </message>
+    <message>
+        <source>Go</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation>主页</translation>
+    </message>
+    <message>
+        <source>Support</source>
+        <translation>支持</translation>
+    </message>
+    <message>
+        <source>Developer</source>
+        <translation>开发者</translation>
+    </message>
+    <message>
+        <source>Frames</source>
+        <translation>帧率</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>捐助</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>翻译</translation>
+    </message>
+    <message>
+        <source>Sourcecode</source>
+        <translation>源代码</translation>
+    </message>
+    <message>
+        <source>Show in systray</source>
+        <translation>显示系统托盘图标</translation>
+    </message>
+    <message>
+        <source>New version available</source>
+        <translation>有新版本可用</translation>
+    </message>
+    <message>
+        <source>Videocodec</source>
+        <translation>视频编码</translation>
+    </message>
+    <message>
+        <source>Audiocodec</source>
+        <translation>音频编码</translation>
+    </message>
+    <message>
+        <source>Webcam</source>
+        <translation>摄像头</translation>
+    </message>
+    <message>
+        <source>Select webcam</source>
+        <translation>选择摄像头</translation>
+    </message>
+    <message>
+        <source>Mirrored</source>
+        <translation>旋转</translation>
+    </message>
+    <message>
+        <source>Display</source>
+        <translation>屏幕</translation>
+    </message>
+    <message>
+        <source>All Displays</source>
+        <translation>所有屏幕</translation>
+    </message>
+    <message>
+        <source>Developer Homepage</source>
+        <translation>开发者主页</translation>
+    </message>
+    <message>
+        <source>Recorder</source>
+        <translation>采集器</translation>
+    </message>
+    <message>
+        <source>Select recorder</source>
+        <translation>选择采集器</translation>
+    </message>
+    <message>
+        <source>Hide window</source>
+        <translation>隐藏窗口</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <source>Show window</source>
+        <translation>显示窗口</translation>
+    </message>
+    <message>
+        <source>Showkey</source>
+        <translation>按键特效</translation>
+    </message>
+    <message>
+        <source>Showclick</source>
+        <translation>点击特效</translation>
+    </message>
+</context>
+</TS>
